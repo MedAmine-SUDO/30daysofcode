ef45579abe8d8d43d30e3478c3ff197b2b030891
MedAmine-SUDO <mohamed-amine.ben-afia@esprims.ens.tn>
2021-02-21 12:43:57 +0100

    solving day 15

---
+++ day15.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+    #Complete this method
+        new = Node(data)
+        if head:
+            tail = head
+            while tail.next:
+                tail = tail.next
+            tail.next = new
+            return head
+        else:
+            return new
+        
+    
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
